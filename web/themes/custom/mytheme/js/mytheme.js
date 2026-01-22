46219ced4ebcfeb053b49f778f508a152ff78c63
Yuri <yuriseki@gmail.com>
2026-01-22 19:44:11 -0300

    Base Theme.

---
+++ web/themes/custom/mytheme/js/mytheme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * MyTheme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.mytheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
